5cd97914d2b0fd8f54851e4fa139a809a30478ae
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-03-13 20:09:26 +0100

    Added ADD_BOOK to reducer

--- src/components/BookForm/BookForm.js
+++ src/components/BookForm/BookForm.js
@@ -1,13 +1,16 @@
 import { useState } from'react';
+import { useDispatch } from 'react-redux';
 
-const BookForm = ({ addBook }) => {
+const BookForm = () => {
+
+  const dispatch = useDispatch();
 
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
 
     const handleSubmit = e => {
       e.preventDefault();
-      addBook({ title: title, author: author }); // { title, author } - takze zadziala !!!
+      dispatch({ type: 'ADD_BOOK', payload: { title: title, author: author }}); // { title, author } - takze zadziala !!!
       setTitle(""); // do wyzerowania pól
       setAuthor(""); // do wyzerowania pól
     }
